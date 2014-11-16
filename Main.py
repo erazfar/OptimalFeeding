0cc539f2c311f4e9953271012483587a3102054f
ajsteig2 <ajsteig2@illinois.edu>
2014-11-16 16:05:07 -0600

    Added discount calculations; output is now equal to last year's code

--- Main.py
+++ Main.py
@@ -10,10 +10,17 @@ def simulate(lst):
 	start_day = lst["start_day"]
 	end_day = lst["end_day"]
 	diff = lst["diff"]
+	food_cost = lst["food_cost"]
+	facility_cost = lst["facility_cost"]
+	animal_price = lst["animal_price"]
+
+	#calculate discount/day from given discount/yr
+	discount = lst["r_value"]/Decimal(365)	
 
 	# generate lookup tables for max size and rates at each day
 	max_sizes = build_max_size_array(lst["start_day"], lst["end_day"])
 	max_rates = build_max_rate_array(max_sizes)
+	print max_sizes
 
 	# get min/start size, max/end size and calculate number of sizes
 	start_size = max_sizes[start_day]
@@ -28,7 +35,7 @@ def simulate(lst):
 	for curr_day in range(start_day, end_day+Decimal(1)):
 		curr_day = Decimal(curr_day)
 		graph[curr_day] = {}
-		print curr_day
+		# print curr_day
 
 		# from curr_size to end_size for this current day,
 		#	set the min_cost to infinity
@@ -45,7 +52,7 @@ def simulate(lst):
 	# iterate through each day except the last
 	for curr_day in range(start_day, end_day):
 		curr_day = Decimal(curr_day)
-		print curr_day
+		# print curr_day
 		curr_size = start_size
 		end_size = max_sizes[curr_day]
 		next_day_min_costs = graph[curr_day+1]
@@ -60,22 +67,13 @@ def simulate(lst):
 			# get the first day s.t. its max size is >= the current size
 			max_size_day = day_by_size_max_feeding(curr_size, max_sizes)
 
-			# count the number of possible sizes
-			'''
-			This is likely to be changed; via past year's RunSimulation lines 92-101:
-			They appear to linearly approximate between the min and the max,
-				but the max is simply the first max >= the current weight.
-			Also, they use the day_diff in the min feeding cost equation
-				(as do we for now) but this assumes there has
-				been day_diff days held constant at that weight, 
-				which is usually not true.
-			'''
-			num_next_sizes = (max_sizes[max_size_day+Decimal(1)] - max_sizes[max_size_day]) / diff
-
 			# get max size and rates based on the first day
 			#	s.t. the max is >= the current size
-			curr_max_size = max_sizes[max_size_day]
-			curr_max_rate = max_rates[max_size_day]
+			curr_max_size = size_by_size_max_feeding(curr_size, max_sizes)
+			curr_min_size = curr_size
+
+			# count the number of possible sizes
+			num_next_sizes = (curr_max_size - curr_size) / diff
 
 			# count the days between current and the max size day
 			day_diff = curr_day - max_size_day
@@ -83,37 +81,53 @@ def simulate(lst):
 			# calculate the minimum feeding cost based on the day diff
 			curr_min_rate = feeding_rate(day_diff, curr_size)
 
+			# calculate the max feeding rate based on current size
+			curr_max_rate = max_feeding_rate(curr_size)
+
+			# calculate current discount rate
+			curr_discount = discount_factor_value(curr_day - start_day, discount)
+
 			# linearly approximate the feed cost depending on the min and max
 			#	feeding rates
-			slope = (curr_max_rate - curr_min_rate) / (num_next_sizes - Decimal(1))
-
-			# set up the counter, current next size and current cost (edge weight)
-			counter = 0
+			if num_next_sizes == 0:
+				slope = 0 # never used, just a placeholder
+			else:
+				slope = (curr_max_rate - curr_min_rate) / num_next_sizes
+				slope *= curr_discount
+			
+			# set up next size and current cost (edge weight)
 			curr_next_size = curr_size
-			curr_next_cost = curr_cost + curr_min_rate
+			curr_next_cost = curr_cost + curr_min_rate*curr_discount
 
 			# for every next possible weight
-			while counter < num_next_sizes:
+			for i in range(num_next_sizes+Decimal(1)):
 			
 				# get the next size's current min cost
-				next_cost = graph[curr_day+Decimal(1)][curr_next_size]
-			
+				next_cost = next_day_min_costs[curr_next_size]
+
 				# if this cost is less than the current min, swap
 				if curr_next_cost < next_cost:
-					next_day_min_costs[curr_next_size] = Decimal(curr_next_cost)
+					next_day_min_costs[curr_next_size] = curr_next_cost
 
 				# increment for the next iteration
 				curr_next_cost += slope
 				curr_next_size += diff
-				counter += 1
 
 			# increment for next size iteration
 			curr_size += diff
-	
+
+	# print ("day, size, revenue, total_cost, kg_food, profit")
+	print ("day, size, kg_food, feeding_cost, profit")
+	for k,v in graph[end_day].items():
+		revenue = get_revenue(start_day, discount, animal_price, end_day, k)
+		total_cost = food_cost * v
+		rent_cost = 0 # facility_cost * (end_day - start_day)
+		profit = revenue - total_cost - rent_cost
+		print ("%d, %f, %f, %f, %f" % (end_day, k, v, total_cost, profit))
 	return graph
 
 # by default, set end_day to 80
-def main(end_day=80):
+def main(end_day=5):
 	lst = {
 		"start_day": Decimal(1),
 		"end_day": end_day,
