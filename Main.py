993b3cb620f73a80a30688030e55d8611c64578b
ajsteig2 <ajsteig2@illinois.edu>
2014-11-17 00:25:09 -0600

    added Node object to store more info, simply command line argument handling

--- Main.py
+++ Main.py
@@ -1,5 +1,7 @@
 from Equations import *
+from Grapher import *
 
+import sys
 from decimal import *
 import collections
 import math
@@ -24,6 +26,14 @@ def get_y_inv(y, start_size, diff):
 # returns the # of sizes between the given max and min, both inclusive
 def get_num_sizes(max_size, min_size, diff):
 	return int((max_size - min_size)/diff + 1)
+
+class Node(object):
+	def __init__(self, day, size, min_cost=Decimal("Infinity")):
+		self.day = day
+		self.size = size		
+		self.min_cost = min_cost
+		self.min_edge = None
+		self.prev_node = None
 
 def simulate(lst):
 	# unbox lst struct
@@ -51,19 +61,22 @@ def simulate(lst):
 		max_sizes[end_day+Decimal(1)+i] = max_size
 
 	# init the graph structure with the initial day's mincost set to zero
-	graph = [ [Decimal(0) ]
+	graph = [ [Node(start_day, start_size, Decimal(0))] ]
 
 	# iterate through each day except the last
 	for curr_day in range(start_day, end_day+extend_days):
+
 		curr_day = Decimal(curr_day)
 		print("Calculating costs for day: %d" % (curr_day))
 
+		# calculate number of reachable sizes
 		curr_size = start_size
 		end_size = max_sizes[curr_day]
 		next_end_size = max_sizes[curr_day+1]
+		num_sizes = get_num_sizes(next_end_size, start_size, diff)
 
-		# create empty column for the next day which will be replaced with min costs
-		graph.append( [Decimal("Infinity")]*get_num_sizes(next_end_size, start_size, diff) )
+		# create empty column for the next day which will be replaced with min costs		
+		graph.append( [Node(curr_day, get_y_inv(i, start_size, diff)) for i in range(num_sizes)] )
 		
 		# cache the column lookups for this day and the next
 		curr_x = get_x(curr_day, start_day)
@@ -77,7 +90,8 @@ def simulate(lst):
 			# get the precalculated min cost to this size on this day
 			# curr_cost = graph[curr_day][curr_size]
 			curr_y = get_y(curr_size, start_size, diff)
-			curr_cost = curr_day_min_costs[curr_y]
+			curr_node = curr_day_min_costs[curr_y]
+			curr_cost = curr_node.min_cost
 
 			# get the first day s.t. its max size is >= the current size
 			max_size_day = day_by_size_max_feeding(curr_size, max_sizes)
@@ -119,11 +133,14 @@ def simulate(lst):
 			
 				# get the next size's current min cost
 				next_y = get_y(curr_next_size, start_size, diff)
-				next_cost = next_day_min_costs[next_y]
+				next_node = next_day_min_costs[next_y]
+				next_cost = next_node.min_cost
 
-				# if this cost is less than the current min, swap
+				# if this cost is less than the current min, swap vals
 				if curr_next_cost < next_cost:
-					next_day_min_costs[next_y] = curr_next_cost
+					next_node.min_cost = curr_next_cost
+					next_node.prev_node = curr_node
+					next_node.min_edge = (curr_next_cost - curr_cost)/curr_discount
 
 				# increment for the next iteration
 				curr_next_cost += slope
@@ -132,29 +149,32 @@ def simulate(lst):
 			# increment for next size iteration
 			curr_size += diff
 
-	print ("day, size, kg_food, feeding_cost, profit")
-	# for k,v in graph[end_day+extend_days].items():
+	print ("day, size, kg_food, feeding_cost, profit, day_cost, prev_node_size")
+
+	# cache last column in graph
 	last_column = graph[int(end_day + extend_days - 1)]
-	for i in range(len(last_column)):
-		# v = get_y_inv(last_column[i], start_size, diff)
-		v = last_column[i]
-		k = get_y_inv(i, start_size, diff)
-		revenue = get_revenue(start_day, discount, animal_price, end_day+extend_days, k)
-		total_cost = food_cost * v
+
+	# iterate over each element
+	for i in range(len(last_column)):		
+		curr_node = last_column[i]
+		min_cost = curr_node.min_cost
+		curr_size = get_y_inv(i, start_size, diff)
+		revenue = get_revenue(start_day, discount, animal_price, end_day+extend_days, curr_size)
+		total_cost = food_cost * min_cost
 		rent_cost = 0 # facility_cost * (end_day - start_day)
 		profit = revenue - total_cost - rent_cost
 		if (extend_days > Decimal(0)):
 			Pa = animal_price
 			Pl = animal_price
 			oc = opportunity_cost(Pa, max_size, end_day-start_day+Decimal(1),
-				Pl, k, end_day+extend_days, discount)
+				Pl, curr_size, end_day+extend_days, discount)
 			profit -= oc
-		print ("%d, %f, %f, %f, %f" % (end_day+extend_days, k, v, total_cost, profit))
+		print ("%d, %f, %f, %f, %f, %f, %f" % (end_day+extend_days, curr_size, min_cost, total_cost, profit, curr_node.min_edge, curr_node.prev_node.size))
 
 	return graph
 
 # by default, set end_day to 80
-def main(end_day=50, extend_days=0):
+def main(end_day=10, extend_days=0):
 	lst = {
 		"start_day": Decimal(1),
 		"end_day": end_day,
@@ -165,7 +185,11 @@ def main(end_day=50, extend_days=0):
 		"diff": Decimal(0.1),
 		"extend_days": Decimal(extend_days)
 	}
-	return simulate(lst)
+	g = simulate(lst)
+	return g
 
 if __name__ == "__main__":
-	main()
+	if (len(sys.argv) == 2):
+		main(int(sys.argv[1]))
+	elif (len(sys.argv) >= 3):
+		main(int(sys.argv[1]), int(sys.argv[2]))
