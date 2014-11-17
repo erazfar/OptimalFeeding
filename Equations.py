397f51c699189d42c76125b951558d184f21d37a
ajsteig2 <ajsteig2@illinois.edu>
2014-11-17 12:15:05 -0600

    Removed Decimal dependency, replacing with float (+rounding for sizes). Performance increased by 30+ times, days 1 to takes < 2 seconds.

--- Equations.py
+++ Equations.py
@@ -1,11 +1,7 @@
 import math
-from decimal import *
 import collections
 
 #from Functions import day_by_size_max_feeding;
-
-ONEPLACES = Decimal('0.1');
-
 
 ## ad-lib function
 def max_size_by_day(day):
@@ -15,11 +11,9 @@ def max_size_by_day(day):
     size = size.quantize(ONEPLACES);
     return size;
     '''
-    
-    day = Decimal(day)
-    size = Decimal('343.822718') + Decimal('1.8290907') * day
-    size = size.quantize(ONEPLACES)
-    return size;
+
+    size = 343.822718 + 1.8290907 * day
+    return round(size, 1)
 
 
 ## max feeding rate function
@@ -32,41 +26,34 @@ def max_feeding_rate(size):
     size = Decimal(size);
     Q = Decimal('1.004226945') + Decimal('0.0176380855')*(size); ##check value
     return Q;
-    '''
-    getcontext().prec = 10;
-    size = Decimal(size)
-    Q = Decimal(4.33209573) + Decimal(0.00562646041)* size ##check value
+    # '''
+
+    Q = 4.33209573 + 0.00562646041 * size
     return Q
 
 ## min feeding rate function    
 def feeding_rate(day_diff, size):
-    getcontext().prec = 10;
-    day_diff = Decimal(day_diff)
-    size = Decimal(size)
-    c = Decimal('0.032566835') * Decimal(pow(size, Decimal('0.75')));
-    ##print ("c:", c)
+    c = 0.032566835 * pow(size, 0.75);
     a = max_feeding_rate(size) - c;
     ## CAUTIOUS: NEED TO UPDATE THE day_on_adlib function
-    ##print ("a:", a)
-    expo = Decimal(math.exp(Decimal('-1') * Decimal('0.032882286') * day_diff));
-    ##print ("expo", expo)
+    expo = math.exp(-1 * 0.032882286 * day_diff);
     Q = a * expo + c;
-    return Q;
+    return Q
     
     
 def opportunity_cost(Pa, Sa, Ta, Pl, Sl, Tl, discount):
     profit = Pa * Sa
     N = 30
-    part1 = Decimal(1/Decimal(math.exp(discount*Ta)-1))
-    part1 = part1 - Decimal(Decimal(math.exp(-1*(N+1)*discount*Ta))/Decimal(1-math.exp(-1*discount*Ta)))
-    part2 = Decimal(1/Decimal(math.exp(discount*Tl)-1))
-    part2 = part2 - Decimal(Decimal(math.exp(-1*(N+1)*discount*Tl))/Decimal(1-math.exp(-1*discount*Tl)))
+    part1 = 1/(math.exp(discount*Ta)-1)
+    part1 -= math.exp(-1*(N+1)*discount*Ta) / (1 - math.exp(-1*discount*Ta))
+    part2 = 1/(math.exp(discount*Tl)-1)
+    part2 -= math.exp(-1*(N+1)*discount*Tl) / (1 - math.exp(-1*discount*Tl))
     dis = profit * (part1-part2)
-    return dis;
+    return dis
     
 
 def discount_factor_value(day, discount):
-    value = Decimal(math.exp(Decimal(-1) * discount * (day)))
+    value = math.exp(-1. * discount * day)
     return value
 
 def get_revenue(start_day, discount, sell_price, w_day, w_size):    
@@ -76,57 +63,47 @@ def get_revenue(start_day, discount, sell_price, w_day, w_size):
     
 def build_facililty_array(cost_array, discount, start_day, end_day):
     facility_array = {}
-    for today in range(start_day, end_day + Decimal('1')):
+    for today in range(start_day, end_day + 1):
         diff = today - start_day
-        today_cost = cost_array[Decimal(today)] * discount_factor_value(diff,discount)
+        today_cost = cost_array[today] * discount_factor_value(diff,discount)
         if diff==0 :
-            facility_array[Decimal(today)] = today_cost
+            facility_array[today] = today_cost
         else :
-            facility_array[Decimal(today)] = today_cost + facility_array[Decimal(today)-Decimal('1')]
+            facility_array[today] = today_cost + facility_array[today-1]
     return facility_array
     
     ############################DO NOT NEED TO MODIFY##################
     
-def build_max_rate_array(max_sizes):
-    max_rate_array = {};
-    
-    for day,size in max_sizes.items():
-        max_rate_array[day] = max_feeding_rate(size)
-
-    return max_rate_array
 
 def build_max_size_array(start_day, end_day):
     max_size_array = {};
     
     for today in range(start_day, end_day + 1):
-        max_size_array[Decimal(today)] = max_size_by_day(Decimal(today))
+        max_size_array[today] = max_size_by_day(today)
     max_size_array = collections.OrderedDict(sorted(max_size_array.items()))
     return max_size_array
     
 ##need to check the correctness of the logic
 def day_by_size_max_feeding(size,max_size_array):
+    ret = None
     for today in max_size_array:
         if(max_size_array[today] >= size):
-        #and today <= end_day):
-               return today;
-    print 'error in day_by_size_max_feeding'
-    return -1;
+               ret = today
+               break
+    return ret
 
 
 
 ##input size and get the size by max feeding
 def size_by_size_max_feeding(size, max_size_array):
-    size = Decimal(size);
-    ##getcontext().prec = 6;
+    ret = None
     for today in max_size_array:
         if (max_size_array[today] >= size):
-            # and today <= (end_day)):
             ## finding the slope of current day ##
-            tempint = (max_size_array[(today+Decimal(1))] - max_size_array[(today)]);
-            
-            return (size + tempint).quantize(ONEPLACES)
-    print 'error in size_by_size_max_feeding'
-    return -1
+            tempint = (max_size_array[today+1] - max_size_array[today]);
+            ret = round(size + tempint, 1)
+            break
+    return ret
 
 
 
--- Main.py
+++ Main.py
@@ -1,7 +1,6 @@
 from Equations import *
 
 import sys
-from decimal import *
 import collections
 import math
 import numpy as np
@@ -9,23 +8,23 @@ import csv
 
 # transforms the given day to the x-coordinate of the graph
 def get_x(day, start_day):
-	return int(day-start_day)
+	return day-start_day
 
 # undoes the above transform
 def get_x_inv(x, start_day):
-	return Decimal(x) + start_day
+	return x + start_day
 
 # transforms the given size to the y-coordinate of the graph
 def get_y(size, start_size, diff):
-	return int((size - start_size)/diff)
+	return int(round((size - start_size)/diff))
 
 # undoes the above transform
 def get_y_inv(y, start_size, diff):
-	return diff*Decimal(y) + start_size
+	return round(diff*y + start_size, 1)
 
 # returns the # of sizes between the given max and min, both inclusive
 def get_num_sizes(max_size, min_size, diff):
-	return int((max_size - min_size)/diff + 1)
+	return int(round((max_size - min_size)/diff)) + 1
 
 # writes the facility cost per day to a CSV file
 def write_facility_cost_csv(facility_cost, start_day, end_day):
@@ -49,29 +48,29 @@ def write_animal_weight_csv(animal_weight, animal_price):
 
 
 class Node(object):
-	def __init__(self, day, size, min_cost=Decimal("Infinity")):
+	def __init__(self, day, size, min_cost=float("inf")):
 		self.day = day
 		self.size = size		
 		self.min_cost = min_cost
-		self.min_edge = Decimal(0)
+		self.min_edge = 0.
 		self.prev_node = None
 
 def simulate(lst):
 	# unbox lst struct
 	start_day = lst["start_day"]
 	end_day = lst["end_day"]
-	diff = lst["diff"]
+	# diff = lst["diff"]
+	diff = 0.1
 	food_cost = lst["food_cost"]
 	facility_cost = lst["facility_cost"]
 	animal_price = lst["animal_price"]
 	extend_days = lst["extend_days"]
 
 	#calculate discount/day from given discount/yr
-	discount = lst["r_value"]/Decimal(365)	
+	discount = lst["r_value"]/365.	
 
-	# generate lookup tables for max size and rates at each day
-	max_sizes = build_max_size_array(lst["start_day"], lst["end_day"])
-	max_rates = build_max_rate_array(max_sizes)
+	# generate lookup tables for max size at each day
+	max_sizes = build_max_size_array(start_day, end_day)
 
 	# get min/start size, max/end size and calculate number of sizes
 	start_size = max_sizes[start_day]
@@ -79,15 +78,15 @@ def simulate(lst):
 
 	# repeat end size for each extend day
 	for i in range(extend_days):
-		max_sizes[end_day+Decimal(1)+i] = max_size
+		max_sizes[end_day+1+i] = max_size
 
 	# init the graph structure with the initial day's mincost set to zero
-	graph = [ [Node(start_day, start_size, Decimal(0))] ]
+	graph = [ [Node(start_day, start_size, 0.)] ]
 
 	# iterate through each day except the last
 	for curr_day in range(start_day, end_day+extend_days):
 
-		curr_day = Decimal(curr_day)
+		curr_day = curr_day
 		print("Calculating costs for day: %d" % (curr_day))
 
 		# calculate number of reachable sizes
@@ -97,7 +96,7 @@ def simulate(lst):
 		num_sizes = get_num_sizes(next_end_size, start_size, diff)
 
 		# create empty column for the next day which will be replaced with min costs		
-		graph.append( [Node(curr_day, get_y_inv(i, start_size, diff)) for i in range(num_sizes)] )
+		graph.append( [Node(curr_day+1, get_y_inv(i, start_size, diff)) for i in range(num_sizes)] )
 		
 		# cache the column lookups for this day and the next
 		curr_x = get_x(curr_day, start_day)
@@ -106,10 +105,9 @@ def simulate(lst):
 
 		# from start to max size that day
 		while curr_size <= end_size:
-			curr_size = Decimal(curr_size)
+			curr_size = curr_size
 
 			# get the precalculated min cost to this size on this day
-			# curr_cost = graph[curr_day][curr_size]
 			curr_y = get_y(curr_size, start_size, diff)
 			curr_node = curr_day_min_costs[curr_y]
 			curr_cost = curr_node.min_cost
@@ -123,7 +121,7 @@ def simulate(lst):
 			curr_min_size = curr_size
 
 			# count the number of possible sizes
-			num_next_sizes = (curr_max_size - curr_size) / diff
+			num_next_sizes = get_num_sizes(curr_max_size, curr_size, diff) # int(round((curr_max_size - curr_size)/ diff))
 
 			# count the days between current and the max size day
 			day_diff = curr_day - max_size_day
@@ -139,7 +137,7 @@ def simulate(lst):
 
 			# linearly approximate the feed cost depending on the min and max
 			#	feeding rates
-			if num_next_sizes == 0:
+			if num_next_sizes == 1:
 				slope = 0 # never used, just a placeholder
 			else:
 				slope = (curr_max_rate - curr_min_rate) / num_next_sizes
@@ -150,7 +148,7 @@ def simulate(lst):
 			curr_next_cost = curr_cost + curr_min_rate*curr_discount
 
 			# for every next possible weight
-			for i in range(num_next_sizes+Decimal(1)):
+			for i in range(num_next_sizes):
 			
 				# get the next size's current min cost
 				next_y = get_y(curr_next_size, start_size, diff)
@@ -161,7 +159,7 @@ def simulate(lst):
 				if curr_next_cost < next_cost:
 					next_node.min_cost = curr_next_cost
 					next_node.prev_node = curr_node
-					next_node.min_edge = (curr_next_cost - curr_cost)/curr_discount
+					next_node.min_edge = (curr_next_cost - curr_cost)/curr_discount					
 
 				# increment for the next iteration
 				curr_next_cost += slope
@@ -169,11 +167,12 @@ def simulate(lst):
 
 			# increment for next size iteration
 			curr_size += diff
+			curr_size = round(curr_size, 1)
 
 	print ("day, size, kg_food, feeding_cost, profit, day_cost, prev_node_size")
 
 	# cache last column in graph
-	last_column = graph[int(end_day + extend_days - 1)]
+	last_column = graph[int(end_day + extend_days - start_day)]
 
 	# iterate over each element
 	for i in range(len(last_column)):
@@ -188,10 +187,10 @@ def simulate(lst):
 
 		# extend_days is greater than zero, calculate and subtract the
 		#	opportunity cost
-		if (extend_days > Decimal(0)):
+		if (extend_days > 0):
 			Pa = animal_price
 			Pl = animal_price
-			oc = opportunity_cost(Pa, max_size, end_day-start_day+Decimal(1),
+			oc = opportunity_cost(Pa, max_size, end_day-start_day+1,
 				Pl, curr_node.size, curr_node.day, discount)
 			profit -= oc
 		print ("%d, %f, %f, %f, %f, %f, %f" % (curr_node.day, curr_node.size, curr_node.min_cost, total_cost, profit, curr_node.min_edge, curr_node.prev_node.size))
@@ -202,24 +201,23 @@ def simulate(lst):
 	return graph
 
 # by default, set end_day to 80
-def main(end_day=80, extend_days=0):
+def main(start_day=1, end_day=80, extend_days=0):
 	lst = {
-		"start_day": Decimal(1),
+		"start_day": start_day,
 		"end_day": end_day,
-		"animal_price":Decimal(2.89),
-		"food_cost":Decimal(0.25),
-		"facility_cost":Decimal(0.35),
-		"r_value":Decimal(0.075),
-		"diff": Decimal(0.1),
-		"extend_days": Decimal(extend_days)
+		"animal_price":2.89,
+		"food_cost":0.25,
+		"facility_cost":0.35,
+		"r_value":0.075,
+		"extend_days": extend_days
 	}
 	g = simulate(lst)
 	return g
 
 if __name__ == "__main__":
-	if (len(sys.argv) == 2):
-		main(int(sys.argv[1]))
-	elif (len(sys.argv) >= 3):
+	if (len(sys.argv) == 3):
 		main(int(sys.argv[1]), int(sys.argv[2]))
+	elif (len(sys.argv) >= 4):
+		main(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]))
 	else:
 		main()
